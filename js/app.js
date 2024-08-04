83418dc572242c93f23e2549a54e9a248e812bfe
Georgina Morales <140033356+Gmoralesmuller@users.noreply.github.com>
2024-08-03 23:18:29 -0500

    feature: desarrollar problema 4

--- js/app.js
+++ js/app.js
@@ -80,12 +80,17 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+    let sumar1 = 0;
+    for (let i = 0; i < sumArr.length; i++){
+      sumar1 = sum(sumar1, sumArr[i])[0];
+    }
+    const mensaje = testArray[0] + ',' + testArray[1] + ',' + testArray[2] + ' was passed in as an array of numbers, and ' + sumar1 + ' is their sum.';
+    return [sumar1, mensaje];
 }
-
+sumArray(testArray);
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
